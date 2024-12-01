86393160b60601b4166c477765d6416cdd56ca76
shafi <shariyarshafi27@gmail.com>
2024-12-01 12:33:29 +0600

    smell: in some exception handling the exception type is not printed.. refactor: printed the exception type. smell:commenting, refactoring: remove the unnecessary commenting

--- searching/GrepContent.java
+++ searching/GrepContent.java
@@ -25,7 +25,7 @@ public class GrepContent {
             ++i;
         } while (openBraces == 0 || openBraces != closedBraces);
         }catch(Exception e){
-        //  System.out.println(e);
+        System.out.println(e);
         }
         result = fileContent.substring(start, i);
 
@@ -33,26 +33,7 @@ public class GrepContent {
         return result;
     }
 
-    /* public int grepLineNumber(String word, String file,int preLine) throws FileNotFoundException, IOException {
-        String str;
-        int linenumber = 0;
-        BufferedReader buf = new BufferedReader(new InputStreamReader(new DataInputStream(new FileInputStream(file))));
-      
-        String line;
-        int LineNumber = 0;
-        while ((line = buf.readLine()) != null) {
-            LineNumber++;
-            //     System.out.println("pop");
-            if (line.contains(word)) {
-                //System.out.println("po" + LineNumber);
-                linenumber = LineNumber;
-                break;
-               // System.out.println("" + linenumber);
-            }
-        }
-        //   System.out.println(""+LineNumber);
-        return linenumber;
-    }*/
+
     public int getLineNumber(String word, String file, int preLine) throws FileNotFoundException, IOException {
     //    System.out.println(word);
         int lineNumber = 0;
